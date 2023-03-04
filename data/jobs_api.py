8acad761ec74d5b45addb10fe5f21d64e32c5dac
Galina24fil <118717385+Galina24fil@users.noreply.github.com>
2023-03-04 14:25:06 +0300

    Получение одной работы
    Получение одной работы

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -1,8 +1,8 @@
 import flask
-
 from . import db_session
 from . jobs import Jobs
 from flask import jsonify
+
 
 blueprint = flask.Blueprint(
     'jobs_api',
@@ -10,14 +10,17 @@ blueprint = flask.Blueprint(
     template_folder='templates'
 )
 
-@blueprint.route('/api/jobs')
-def get_jobs():
+
+@blueprint.route('/api/jobs/<int:job_id>', methods=['GET'])
+def get_jobs(job_id):
     db_sess = db_session.create_session()
-    jobs = db_sess.query(Jobs).all()
+    jobs = db_sess.query(Jobs).get(job_id)
+    if not jobs:
+        return jsonify({'error': 'Not found'})
     return jsonify(
         {
-            'jobs':
-                [item.to_dict(only=('team_leader', 'job', 'work_size', 'collaborators', 'start_date', 'end_date', 'is_finished'))
-                 for item in jobs]
+            'jobs': jobs.to_dict(only=(
+                'id', 'team_leader', 'job', 'work_size', 'collaborators', 'start_date', 'end_date', 'is_finished'))
         }
     )
+
--- mmain.py
+++ mmain.py
@@ -1,8 +1,15 @@
 from data import db_session, jobs_api
 from flask import Flask
+from flask import make_response
+from flask import jsonify
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 def main():
     db_session.global_init("db/blogs.db")
