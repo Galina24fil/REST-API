ba226c0dede11be4282b09a5c5c8a430e3f33c1d
Galina24fil <118717385+Galina24fil@users.noreply.github.com>
2023-03-04 14:01:28 +0300

    Получение всех работ
    Получение всех работ

---
+++ mmain.py
@@ -0,0 +1,14 @@
+from data import db_session, jobs_api
+from flask import Flask
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+def main():
+    db_session.global_init("db/blogs.db")
+    app.register_blueprint(jobs_api.blueprint)
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
