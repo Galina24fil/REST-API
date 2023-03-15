e93f916c6a7d3f3436d0ea1440354a871de948ff
Galina24fil <118717385+Galina24fil@users.noreply.github.com>
2023-03-15 17:41:35 +0300

    Update users_api.py

--- data/users_api.py
+++ data/users_api.py
@@ -1,16 +1,16 @@
 import flask
 from . import db_session
-from . users import User
+from .users import User
 from flask import jsonify
 from flask import request
 from werkzeug.security import generate_password_hash
-
 
 blueprint = flask.Blueprint(
     'users_api',
     __name__,
     template_folder='templates'
 )
+
 
 @blueprint.route('/api/users')
 def get_users():
@@ -19,10 +19,12 @@ def get_users():
     return jsonify(
         {
             'users':
-                [item.to_dict(only=('id', 'surname', 'name', 'age', 'position', 'speciality', 'address', 'email', 'password_hash'))
+                [item.to_dict(only=(
+                'id', 'surname', 'name', 'age', 'position', 'speciality', 'address', 'email', 'password_hash'))
                  for item in users]
         }
     )
+
 
 @blueprint.route('/api/users/<int:user_id>', methods=['GET'])
 def get_users1(user_id):
@@ -37,32 +39,32 @@ def get_users1(user_id):
         }
     )
 
+
 @blueprint.route('/api/users', methods=['POST'])
 def get_users2():
+    db_sess = db_session.create_session()
     if not request.json:
         return jsonify({'error': 'Empty request'})
     elif not all(key in request.json for key in
-                 ['id', 'surname', 'name', 'age', 'position', 'speciality', 'address', 'email', 'password']):
+                 ['surname', 'name', 'age', 'position', 'speciality', 'address', 'email', 'password']):
         return jsonify({'error': 'Bad request'})
-    db_sess = db_session.create_session()
-    s = db_sess.query(User).filter(User.id == request.json['id']).first()
-    if not s:
-        users = User(
-            id=request.json['id'],
-            surname=request.json['surname'],
-            name=request.json['name'],
-            age=request.json['age'],
-            position=request.json['position'],
-            speciality=request.json['speciality'],
-            address=request.json['address'],
-            email=request.json['email']
-        )
-        users.password_hash = generate_password_hash(request.json['password'])
-        db_sess.add(users)
-        db_sess.commit()
-        return jsonify({'success': 'OK'})
-    else:
-        return jsonify({'error': 'Id already exists'})
+    elif 'id' in request.json:
+        s = db_sess.query(User).filter(User.id == request.json['id']).first()
+        if s:
+            return jsonify({'error': 'Id already exists'})
+    users = User(
+        surname=request.json['surname'],
+        name=request.json['name'],
+        age=request.json['age'],
+        position=request.json['position'],
+        speciality=request.json['speciality'],
+        address=request.json['address'],
+        email=request.json['email']
+    )
+    users.password_hash = generate_password_hash(request.json['password'])
+    db_sess.add(users)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
 
 
 @blueprint.route('/api/users/<int:users_id>', methods=['DELETE'])
@@ -75,12 +77,13 @@ def delete_users(users_id):
     db_sess.commit()
     return jsonify({'success': 'OK'})
 
+
 @blueprint.route('/api/users/<int:users_id>', methods=['POST'])
 def edit_jobs(users_id):
     if not request.json:
         return jsonify({'error': 'Empty request'})
     elif not all(key in request.json for key in
-                 ['id', 'surname', 'name', 'age', 'position', 'speciality', 'address', 'email', 'password']):
+                 ['surname', 'name', 'age', 'position', 'speciality', 'address', 'email', 'password']):
         return jsonify({'error': 'Bad request'})
     db_sess = db_session.create_session()
     s = db_sess.query(User).filter(User.id == users_id).first()
@@ -91,7 +94,7 @@ def edit_jobs(users_id):
         db_sess.delete(jobs)
         db_sess.commit()
         users = User(
-            id=request.json['id'],
+            id=users_id,
             surname=request.json['surname'],
             name=request.json['name'],
             age=request.json['age'],
