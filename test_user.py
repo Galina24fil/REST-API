5e2ee97aaff917f67c864418b876361c6ac75e8a
Galina24fil <118717385+Galina24fil@users.noreply.github.com>
2023-03-15 17:43:02 +0300

    Update test_user.py

--- test_user.py
+++ test_user.py
@@ -2,17 +2,16 @@ from requests import get, post, delete
 
 print(get('http://localhost:5000/api/users').json())
 print(get('http://localhost:5000/api/users/5').json())
-#print(delete('http://localhost:5000/api/users/1').json())
+# print(delete('http://localhost:5000/api/users/1').json())
 print(post('http://localhost:5000/api/users/5',
-           json={'id': 5,
-            'surname': 'Фамилия',
-            'name': 'name',
-            'age': 2,
-            'position': 'position',
-            'speciality': 'speciality',
-            'address': 'address',
-            'email': 'email5@email',
-            'password': 'Qazqwer5'}).json())
+           json={'surname': 'Фамилия',
+                 'name': 'name',
+                 'age': 2,
+                 'position': 'position',
+                 'speciality': 'speciality',
+                 'address': 'address',
+                 'email': 'email5@email',
+                 'password': 'Qazqwer5'}).json())
 
 print(get('http://localhost:5000/api/users').json())
 
